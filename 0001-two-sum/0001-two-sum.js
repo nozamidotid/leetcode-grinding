fa4045e4ae6f3bb26c175d84dbbdc4a11e1b8489
nozamidotid <131259221+nozamidotid@users.noreply.github.com>
2024-03-14 16:04:25 +0700

    Time: 56 ms (80.95%), Space: 51.1 MB (31.80%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -4,14 +4,14 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    let map = new Map();
+    let map = {}
     for (let i = 0; i < nums.length; i++) {
-        if(map.has(target - nums[i])) {
-            return [map.get(target - nums[i]), i];
-        } else {
-            map.set(nums[i], i);
+        let sisa = target - nums[i]
+
+        if(map[sisa] !== undefined){
+            return [map[sisa], i]
         }
+
+        map[nums[i]] = i
     }
-    
-    return []
 };
